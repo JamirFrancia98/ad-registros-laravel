2e3832a36fa0aa589852ab1aed9d14b39004d61e
Danny Olivera <dannyoliveradev@gmail.com>
2026-02-11 19:09:10 -0500

    feat: updaet

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,23 +17,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call([
-        //     StorageOptionsSeeder::class,
-        //     IphoneModelsSeeder::class,
-        //     ColorsSeeder::class,
-        //     SuppliersSeeder::class,
-        // ]);
-        // Customer::factory(10)->create();
-        // Purchase::factory(10)->create();
-        // User::query()->create([
-        //     'name' => 'Francia Jamir',
-        //     'email' => 'franciajamir@gmail.com',
-        //     'password' => bcrypt('12345'),
-        // ]);
-        // User::query()->create([
-        //     'name' => 'Danny Olivera',
-        //     'email' => 'dannyoliveradev@gmail.com',
-        //     'password' => bcrypt('12345678'),
-        // ]);
+        $this->call([
+            StorageOptionsSeeder::class,
+            IphoneModelsSeeder::class,
+            ColorsSeeder::class,
+            SuppliersSeeder::class,
+        ]);
+        Customer::factory(10)->create();
+        Purchase::factory(10)->create();
+        User::query()->create([
+            'name' => 'Francia Jamir',
+            'email' => 'franciajamir@gmail.com',
+            'password' => bcrypt('12345'),
+        ]);
+        User::query()->create([
+            'name' => 'Danny Olivera',
+            'email' => 'dannyoliveradev@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
     }
 }
